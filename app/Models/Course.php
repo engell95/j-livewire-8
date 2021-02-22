35d8d7022c86cb59c611992e1fa927c524e706e7
Engell Lopez <engelllopez59@gmail.com>
2021-02-21 18:37:35 -0600

    Livewire: diseño del componente

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -12,4 +12,8 @@ class Course extends Model
     public function user(){
         return $this->belongsTo(User::Class);
     }
+
+    public function getExcerptAttribute(){
+        return substr($this->description,0,80)."...";
+    }
 }
--- resources/views/livewire/course-list.blade.php
+++ resources/views/livewire/course-list.blade.php
@@ -1,5 +1,12 @@
-<div>
+<div class="grid grid-cols-3 gap-4 mt-8">
     @foreach($Course as $item)
-        <label>{{$item->id}}</label>
+        <div class="bg-white shadow-lg rounded-lg px-4 py-6 text-center">
+            <a href="#">
+                <img src="{{$item->image}}" class="rounded-md mb-2">
+                <h2 class="text-lg text-gray-600 truncate">{{$item->name}}</h2>
+                <h3>{{$item->excerpt}}</h3>
+                <img src="{{$item->user->avatar}}" class="rounded-full mt-4 mx-auto h-16 w-16">
+            </a>
+        </div>
     @endforeach
 </div>
