2b6d5d2b1ca7c9ce1eab6c8220042e9b69e90e45
Engell Lopez <engelllopez59@gmail.com>
2021-02-21 20:29:19 -0600

    componentes

---
+++ resources/views/components/course-card.blade.php
@@ -0,0 +1,8 @@
+<div class="bg-white shadow-lg rounded-lg px-4 py-6 text-center">
+    <a href="{{route('course',$item->slug)}}">
+        <img src="{{$item->image}}" class="rounded-md mb-2">
+        <h2 class="text-lg text-gray-600 truncate">{{$item->name}}</h2>
+        <h3>{{$item->excerpt}}</h3>
+        <img src="{{$item->user->avatar}}" class="rounded-full mt-4 mx-auto h-16 w-16">
+    </a>
+</div>
--- resources/views/livewire/course-list.blade.php
+++ resources/views/livewire/course-list.blade.php
@@ -1,12 +1,5 @@
 <div class="grid grid-cols-3 gap-4 mt-8">
     @foreach($Course as $item)
-        <div class="bg-white shadow-lg rounded-lg px-4 py-6 text-center">
-            <a href="{{route('course',$item->slug)}}">
-                <img src="{{$item->image}}" class="rounded-md mb-2">
-                <h2 class="text-lg text-gray-600 truncate">{{$item->name}}</h2>
-                <h3>{{$item->excerpt}}</h3>
-                <img src="{{$item->user->avatar}}" class="rounded-full mt-4 mx-auto h-16 w-16">
-            </a>
-        </div>
+        <x-course-card :item="$item" />
     @endforeach
 </div>
